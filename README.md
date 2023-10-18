603a4fb9f2c1d01c01c6146ffc68ea8472888fcd
clopso <caiolemes306@gmail.com>
2023-10-18 10:27:59 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# nRF5340 DK
+
+This repository contains examples for the nRF5340 DK.
+
+## Folders
+
+| Folder              | Description    |
+| ------------------- | -------------- |
+| [blinky](./blinky/) | Blinky example |
+
+## Links
+
+- [nRF5340 DK product page](https://www.nordicsemi.com/Products/Development-hardware/nRF5340-DK)
+- [nRF Command Line Tools](https://www.nordicsemi.com/Products/Development-tools/nRF-Command-Line-Tools)
+- [nRF Connect SDK](https://www.nordicsemi.com/Software-and-tools/Software/nRF-Connect-SDK)
---
+++ blinky/src/main.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   500
+
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+
+
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
+
+int main(void)
+{
+	gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+
+	int flag = 0;
+	int flagArray[4] = {0};
+
+	while (1) {
+		flagArray[flag] = 1;
+		gpio_pin_set_dt(&led0, flagArray[0]);
+		gpio_pin_set_dt(&led1, flagArray[1]);
+		gpio_pin_set_dt(&led3, flagArray[2]);
+		gpio_pin_set_dt(&led2, flagArray[3]);
+		flagArray[flag] = 0;
+		flag++;
+		if(flag == 4) flag = 0;
+
+		k_msleep(SLEEP_TIME_MS);
+	}
+
+	return 0;
+}
